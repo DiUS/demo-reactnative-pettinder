b880810fda88dd91e7bce50daaeddd9e9c3b6163
Trevor Plant <tplant@dius.com.au>
2020-08-09 10:43:57 +1000

    Login and register flow

---
+++ src/screens/HomeScreen/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function HomeScreen(props) {
+    const {extraData} = props;
+    console.log('Home Screen: ', {extraData});
+    return (
+        <View>
+            <Text>Home Screen, Welcome {extraData.fullName}</Text>
+        </View>
+    )
+}
